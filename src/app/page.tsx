d789ec7ad108129dd1cb1682b43b43bae57872cb
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-06-26 15:46:57 -0700

    Blend mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Index() {
                 >
                     <div className="absolute left-[40%] top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 transform text-left text-9xl text-white mix-blend-screen md:left-[50%]">
                         <div className="font-light tracking-tighter">
-                            <div className="bg-gradient-to-t from-teal-400/100 to-indigo-500/100 bg-clip-text text-transparent drop-shadow-2xl">
+                            <div className="bg-gradient-to-t from-teal-400/95 to-indigo-500/95 bg-clip-text text-transparent drop-shadow-2xl">
                                 <div className="font-bold italic">Hi.</div>
                                 <div className="italic">I'm Chris.</div>
                             </div>
