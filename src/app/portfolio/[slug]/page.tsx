214e174becb0a9813444a52e49f48d405e9a1dca
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-06-27 20:14:40 -0700

    Add a placeholder blog

--- src/app/portfolio/[slug]/page.tsx
+++ src/app/portfolio/[slug]/page.tsx
@@ -3,7 +3,6 @@ import { notFound } from 'next/navigation';
 import { getAllPortfolio, getPortfolioBySlug } from '@/lib/api';
 import { CMS_NAME } from '@/lib/constants';
 import markdownToHtml from '@/lib/markdownToHtml';
-import Alert from '@/app/_components/alert';
 import Container from '@/app/_components/container';
 import Header from '@/app/_components/header';
 import { PostBody } from '@/app/_components/post-body';
@@ -19,8 +18,7 @@ export default async function Post({ params }: Params) {
     const content = await markdownToHtml(post.content || '');
 
     return (
-        <main>
-            <Alert preview={post.preview} />
+        <main className='p-10'>
             <Container>
                 <Header text={'Portfolio.'} />
                 <article className="mb-32">
