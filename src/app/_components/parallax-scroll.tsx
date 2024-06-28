48faafd24553ac551035bc763094645189a5d709
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-06-27 23:24:36 -0700

    Changing colors

--- src/app/_components/parallax-scroll.tsx
+++ src/app/_components/parallax-scroll.tsx
@@ -141,7 +141,7 @@ export const ParallaxScroll = ({
     const translateSecond = useTransform(
         scrollYProgress,
         [0.23, 0.7],
-        [0, 142]
+        [0, 140]
     );
     const translateThird = useTransform(
         scrollYProgress,
