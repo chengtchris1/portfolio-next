4b4ce7646c683535519b09e2a0dc299d9e29b58e
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-07-05 21:34:40 -0700

    fix window

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -13,26 +13,9 @@ const greetings = [
 ];
 export default function Hero() {
     return (
-        <div className="flex items-start justify-center h-screen overflow-hidden mb-20">
-            <div className="absolute left-[56%] top-[60%] z-50 -translate-x-1/2 -translate-y-1/2 transform text-left text-6xl text-white mix-blend-screen md:left-[60%] lg:left-[60%] md:text-8xl lg:text-9xl w-[90%] max-w-[80%] px-0 mx-0">
-                <div className="font-light tracking-tighter">
-                    <div className="bg-transparent bg-clip-text text-transparent drop-shadow-2xl">
-                        {/*<div className="font-bold text-blue italic ">
-                            <FlipWords
-                                className="opacity-60 "
-                                words={greetings}
-                                duration={500}
-                            />
-                        </div>
-                        <div className="bg-gradient-to-b from-teal-400/95 to-indigo-500/95  bg-clip-text text-transparent drop-shadow-2xl">
-                            <div className="px-2 font-bold">I'm Chris.</div>
-                        </div>
-*/}
-                    </div>
-                </div>
-            </div>
+        <div className="flex items-start justify-center h-[95vh] overflow-hidden mb-20">
             <div className="absolute w-full bg-transparent from-blue-600 to-black  flex items-center justify-center bg-clip-content">
-                <div className="flex items-center justify-center w-5/6 mt-28 overflow-hidden  relative rounded-xl flex-col drop-shadow-2xl">
+                <div className="flex items-center justify-center w-5/6 h-5/6 mt-20 overflow-hidden  relative rounded-xl flex-col drop-shadow-2xl">
                     <div className="bg-slate-950 flex items-start justify-start py-[1rem] gap-2 p-4 pl-5 w-full mt-36 sm:mt-0 rounded-t-2xl">
                         <div className="w-3 h-3  bg-red-500 transition duration-300 rounded-full"></div>
                         <div className="w-3 h-3   bg-yellow-500 transition duration-300 rounded-full"></div>
