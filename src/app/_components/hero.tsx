28d8aa3c83affcf266331d7521447a7597a1d24c
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-07-05 18:17:25 -0700

    seperate hero into own component

---
+++ src/app/_components/hero.tsx
@@ -0,0 +1,70 @@
+import Image from 'next/image';
+import { FlipWords } from './filpWords';
+import { BackgroundGradientAnimation } from './animatedGradient';
+import bg from '@/assets/hero-bg.png';
+const greetings = [
+    'Hello,', // English
+    'Hola,', // Spanish
+    'Bonjour,', // French
+    '你好,',
+    'こんにちは,', // Japanese
+    '안녕하세요,', // Korean
+    'Xin chào,', // Vietnamese
+];
+export default function Hero() {
+    return (
+        <div className="flex items-start justify-center h-screen">
+            <div className="absolute left-[62%] top-[60%] z-50 -translate-x-1/2 -translate-y-1/2 transform text-left text-6xl text-white mix-blend-screen md:left-[70%] lg:left-[70%] md:text-8xl lg:text-9xl w-screen">
+                <div className="font-light tracking-tighter">
+                    <div className="bg-transparent bg-clip-text text-transparent drop-shadow-2xl">
+                        <div className="font-bold text-blue italic">
+                            <FlipWords
+                                className="opacity-60"
+                                words={greetings}
+                                duration={500}
+                            />
+                        </div>
+                        <div className="bg-gradient-to-b from-teal-400/95 to-indigo-500/95  bg-clip-text text-transparent drop-shadow-2xl">
+                            <div className="px-2 font-bold">I'm Chris.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="absolute w-full bg-violet-950 flex items-center justify-center bg-clip-content">
+                <div className="flex items-center justify-center w-5/6 mt-28 overflow-hidden  relative rounded-t-xl flex-col">
+                    <div className="bg-slate-950 flex items-start justify-start gap-[0.33rem] py-[.66rem] sm:gap-2 p-4 pl-5 w-full">
+                        <div className="w-3 h-3  bg-red-500 transition duration-300 rounded-full"></div>
+                        <div className="w-3 h-3   bg-yellow-500 transition duration-300 rounded-full"></div>
+                        <div className="w-3 h-3   bg-green-500 rounded-full transition duration-300 "></div>
+                    </div>
+                    <div className="h-[82vh]">
+                        <BackgroundGradientAnimation
+                            gradientBackgroundStart="rgb(0,0,0)"
+                            gradientBackgroundEnd="rgb(0,0,0)"
+                            firstColor="20,184,166"
+                            // secondColor="255,255,255"
+                            thirdColor="20,184,166"
+                            fifthColor="56,189,248"
+                            fourthColor="127,0,255"
+                            //pointerColor="255, 255, 255"
+                            interactive={true}
+                            blendingValue="screen"
+                            size="90%"
+                        >
+                          <div className='flex'>
+                            <Image
+                                src={bg}
+                                quality={100}
+                                priority={true}
+                                className="absolute h-full w-full object-fill"
+                                alt={"hero bg"}
+                            />
+                            <div className="absolute inset-0 bg-gradient-to-t from-violet-950 from-25% to-transparent"></div>
+                            </div>
+                        </BackgroundGradientAnimation>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image';
 import { BackgroundGradientAnimation } from './_components/animatedGradient';
 import { FlipWords } from './_components/filpWords';
 import { ParallaxScroll } from './_components/parallax-scroll';
+import Hero from './_components/hero';
 export default function Index() {
     const greetings = [
         'Hello,', // English
@@ -22,57 +23,8 @@ export default function Index() {
     const images = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     return (
         <main>
-            <div className="bg-dark-gradient m-0 mb-0 flex h-fit flex-col-reverse items-center justify-center bg-sky-600 bg-cover bg-center bg-no-repeat object-cover sm:h-screen sm:flex-row">
-                {' '}
-                <BackgroundGradientAnimation
-                    gradientBackgroundStart="rgb(0,0,0)"
-                    gradientBackgroundEnd="rgb(0,0,0)"
-                    firstColor="20,184,166"
-                    // secondColor="255,255,255"
-                    thirdColor="20,184,166"
-                    fifthColor="56,189,248"
-                    fourthColor="127,0,255"
-                    //pointerColor="255, 255, 255"
-                    interactive={true}
-                    blendingValue="color-dodge"
-                    size="90%"
-                >
-                    <div className="absolute left-[62%] top-[60%] z-50 -translate-x-1/2 -translate-y-1/2 transform text-left text-6xl text-white mix-blend-screen md:left-[70%] lg:left-[70%] md:text-8xl lg:text-9xl w-screen">
-                        <div className="font-light tracking-tighter">
-                            <div className="bg-transparent bg-clip-text text-transparent drop-shadow-2xl">
-                                <div className="font-bold text-blue italic">
-                                    <FlipWords
-                                        className="opacity-60"
-                                        words={greetings}
-                                        duration={500}
-                                    />
-                                </div>
-                                <div className="bg-gradient-to-b from-teal-400/95 to-indigo-500/95  bg-clip-text text-transparent drop-shadow-2xl">
-                                    <div className="px-2 font-bold">
-                                        I'm Chris.
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-full bg-violet-950 flex items-center justify-center bg-clip-content">
-                        <div className="flex items-center justify-center w-5/6 mt-28 overflow-hidden  relative rounded-t-xl flex-col">
-                            <div className="absolute inset-0 bg-gradient-to-t from-violet-950 from-25% to-transparent"></div>
-                            <div className="bg-slate-950 flex items-start justify-start gap-[0.33rem] py-[.66rem] sm:gap-2 p-2 pl-[.85rem] sm:p-4 sm:pl-5 w-full">
-                                <div className="w-2 h-2 sm:w-3 sm:h-3 bg-gray-500 group-hover:bg-red-500 transition duration-300 rounded-full"></div>
-                                <div className="w-2 h-2 sm:w-3 sm:h-3 bg-gray-500 group-hover:bg-yellow-500 transition duration-300 rounded-full"></div>
-                                <div className="w-2 h-2 sm:w-3 sm:h-3 bg-gray-500 group-hover:bg-green-500 rounded-full transition duration-300 "></div>
-                            </div>
-                            <Image
-                                src={bg}
-                                quality={100}
-                                priority={true}
-                                style={{ objectFit: 'cover' }}
-                                className="h-screen"
-                            />
-                        </div>
-                    </div>
-                </BackgroundGradientAnimation>
+            <div className="bg-dark-gradient m-0 flex h-full flex-col-reverse items-center justify-center bg-sky-600 bg-cover bg-center bg-no-repeat object-cover sm:flex-row mb-0">
+                <Hero />
             </div>
 
             <div className="w-full bg-white text-black">
