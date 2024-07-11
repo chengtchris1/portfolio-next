ce46e12bdffd042d19bd504d383acd5beceb4120
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-07-11 00:34:37 -0700

    font

--- src/app/_components/techStack.tsx
+++ src/app/_components/techStack.tsx
@@ -4,7 +4,7 @@ export default function TechStack({ techStack }: { techStack: string[] }) {
             {techStack?.map((tech) => (
                 <span
                     key={tech}
-                    className="my-1 mr-2 rounded-xl border p-2 text-white hover:text-black bg-transparent hover:bg-white transition duration-300 font-light text-xs"
+                    className="my-1 mr-2 rounded-xl border p-2 text-white hover:text-black bg-transparent hover:bg-white transition duration-300 font-mono text-xs"
                 >
                     {tech}
                 </span>
