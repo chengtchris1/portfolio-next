1f0060243cf9a6e5aaa935f5fca96ccfc724e513
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-07-11 19:41:16 -0700

    remove portfolio text

--- src/app/_components/headerNavbar.tsx
+++ src/app/_components/headerNavbar.tsx
@@ -7,6 +7,7 @@ function HeaderNavbar() {
     const { scrollYProgress } = useScroll();
     const path = usePathname();
     const [isHome, setIsHome] =  useState<boolean | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
     const backgroundColor = useTransform(
         scrollYProgress,
         [0.11, 0.14],
@@ -14,9 +15,10 @@ function HeaderNavbar() {
     );
     useEffect(() => {
         setIsHome(window.location.pathname === '/');
+        setIsLoading(false);
     }, [path]);
     return (
-        <div className="fixed h-10 w-screen px-0 mx-0 z-[1000]">
+        !isLoading && <div className="fixed h-10 w-screen px-0 mx-0 z-[1000]">
             <motion.div
                 className="mx-0 w-full py-5 px-10 z-[1000] flex-row flex justify-between items-center backdrop-blur-none grid-cols-3 gap-0 drop-shadow"
                 style={
